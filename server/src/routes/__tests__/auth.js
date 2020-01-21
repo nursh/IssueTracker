518e702eb8d7d91b2f1c81755aa96a2eac3d4897
nursh <nsheikhmohammed01@gmail.com>
2020-01-20 22:42:16 -0500

    Finished testing local signup path with integration test. Closes #23.

---
+++ server/src/routes/__tests__/auth.js
@@ -0,0 +1,62 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { localLoginForm, buildUserInfo } from 'test/generate';
+import setDB from '../../db';
+
+let user;
+let db;
+
+beforeAll(async () => {
+  await setup();
+});
+
+afterAll(async () => {
+  await db.collection('users').deleteMany({});
+});
+
+async function setup() {
+  const database = setDB();
+  db = await database;
+  const { ops } = await db.collection('users').insertOne(buildUserInfo());
+  user = ops[0];
+}
+
+describe('/auth/signup', () => {
+  test('responds with a token when the userInfo is valid and does not exist in DB', async () => {
+    const newUser = localLoginForm();
+    const response = await request(app)
+      .post('/auth/signup')
+      .send(newUser);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty('token');
+  });
+
+  test('responds with an error when the user already exists', async () => {
+    const response = await request(app)
+      .post('/auth/signup')
+      .send(user);
+
+    expect(response.statusCode).toBe(422);
+    expect(response.body).toMatchInlineSnapshot(`
+      Object {
+        "error": "Email and Password fields must be provided.",
+      }
+    `);
+  });
+
+  test('responds with an error when signing up with incomplete details', async () => {
+    const incompleteUser = localLoginForm({ email: false });
+
+    const response = await request(app)
+      .post('/auth/signup')
+      .send(incompleteUser);
+
+    expect(response.statusCode).toBe(422);
+    expect(response.body).toMatchInlineSnapshot(`
+      Object {
+        "error": "Email and Password fields must be provided.",
+      }
+    `);
+  });
+});
