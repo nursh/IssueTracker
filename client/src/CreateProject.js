2afcc00dce5c2aa46854af0e61d17e6b4e42fe2d
nursh <nsheikhmohammed01@gmail.com>
2020-04-09 14:58:30 -0400

    Add formik to CreateProject component. #73

--- client/src/CreateProject.js
+++ client/src/CreateProject.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 export default function CreateProject({ history }) {
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: ''
+    },
+    onSubmit: values => {
+      console.log(values)
+    },
+    validationSchema: createProjectSchema
+  })
   return (
     <div className="w-72 m-auto shadow px-8 py-10 border z-50 bg-white">
       <h2 className="text-center mb-10 font-semibold text-xl uppercase tracking-wide">
         Create Project
       </h2>
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={formik.handleSubmit}>
         <div className="flex flex-col">
           <label
             htmlFor="title"
@@ -20,7 +32,14 @@ export default function CreateProject({ history }) {
             id="title"
             name="title"
             className="mt-2 py-3 px-4 rounded bg-gray-200 border"
+            onChange={formik.handleChange}
+            value={formik.values.title}
           />
+          {formik.errors.title && formik.touched.title ? (
+            <div className="text-red-500 text-sm px-4">
+              {formik.errors.title}
+            </div>
+          ) : null}
         </div>
 
         <div className="flex flex-col mt-6">
@@ -36,18 +55,31 @@ export default function CreateProject({ history }) {
             name="description"
             rows={4}
             className="mt-2 py-3 px-4 rounded bg-gray-200 border"
+            onChange={formik.handleChange}
+            value={formik.values.description}
           />
         </div>
 
-
-        <button type="submit" className="shadow rounded px-4 py-3 bg-indigo-600 text-white mt-10 hover:bg-indigo-700 uppercase text-sm">
+        <button
+          type="submit"
+          className="shadow rounded px-4 py-3 bg-indigo-600 text-white mt-10 hover:bg-indigo-700 uppercase text-sm"
+        >
           Create Project
         </button>
 
-        <button type="button" onClick={() => history.goBack()} className="shadow rounded px-4 py-3 border mt-4 hover:bg-gray-200 uppercase text-sm">
+        <button
+          type="button"
+          onClick={() => history.goBack()}
+          className="shadow rounded px-4 py-3 border mt-4 hover:bg-gray-200 uppercase text-sm"
+        >
           Cancel
         </button>
       </form>
     </div>
   );
 }
+
+const createProjectSchema = Yup.object().shape({
+  title: Yup.string().required('Title is required'),
+  description: Yup.string()
+});
