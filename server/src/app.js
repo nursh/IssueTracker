cd4dff12f28c966249a8811a673e63fea47a8b49
nursh <nsheikhmohammed01@gmail.com>
2020-05-07 11:49:29 -0400

    Deploying to heroku. Attempt #1.

--- server/src/app.js
+++ server/src/app.js
@@ -35,4 +35,13 @@ app.get('/api/cleardb', async (req, res) => {
   return res.status(200).json({ message: 'Successfully cleared db' });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../../client/build'));
+
+  const path = require('path');
+  app.get('*', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 export { app };
